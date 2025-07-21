da2a31ddbae69b3cdca702f50a727217723afced
AkshatDahal7 <akshatdahal913@gmail.com>
2025-07-21 12:26:24 +0545

    V2.1.2.3

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,17 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
-
-// Convert the current module URL to a file path
-const __filename = fileURLToPath(import.meta.url);
-// Get the directory name from the file path
-const __dirname = path.dirname(__filename);
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@core': path.resolve(__dirname, './core'),
-      '@src': path.resolve(__dirname, './src')
+      '@core': path.resolve(__dirname, 'core'),
+      '@src': path.resolve(__dirname, 'src')
     }
   }
 });
